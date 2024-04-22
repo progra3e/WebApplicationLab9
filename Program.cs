11e403599e6cb28a55def28909306786bd3b192a
gramz <gramz@LAB4-CAT4>
2024-04-22 07:57:01 -0600

    Guarda la lista de inscripciones de forma local

--- Program.cs
+++ Program.cs
@@ -5,10 +5,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
-
 
 namespace WebApplicationLab9
 {
